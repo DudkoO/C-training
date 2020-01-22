bffe229c6cbdecceaa97096bbbf59344e25e3fc8
Dudko Oleg <adsuperandumnatus@gmail.com>
2020-01-22 16:25:21 +0200

    Добавьте файлы проекта.

---
+++ C Sharp Traning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Traning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
